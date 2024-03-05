e2d86f44630c9e5dff9b81501f44dd7c4ce98954
rasm445f <90387951+rasm445f@users.noreply.github.com>
2024-03-05 12:52:44 +0100

    some some file file

--- sql/storedProcedure.sql
+++ sql/storedProcedure.sql
@@ -44,8 +44,13 @@ END;
 EXEC GetCityDetails @CityName = 'Copenhagen'
 
 -----------------------------------------------------------
+IF OBJECT_ID('GetTotalEmissionByCity', 'P') IS NOT NULL
+BEGIN
+    DROP PROCEDURE GetTotalEmissionByCity;
+END
+GO
 
-CREATE PROCEDURE GetCityEmissionsWithStatus
+CREATE PROCEDURE GetTotalEmissionByCity
     @CityName VARCHAR(100)
 AS
 BEGIN
@@ -60,20 +65,52 @@ BEGIN
     -- Aggregate emissions for the most recent year and get the status
     SELECT 
         e.id,
-        SUM(e.totalCityWideEmissionsCO2) AS Total,
-        SUM(e.totalScope1_CO2) AS TotalScope1Emission,
-        SUM(e.totalScope2_CO2) AS TotalScope2Emission,
-        est.type AS Change,
+        e.totalCityWideEmissionsCO2,
+        e.totalScope1_CO2,
+        e.totalScope2_CO2,
+        est.type,
         e.description,
         e.comment
     FROM GHG_Emissions e
     JOIN Organisations o ON e.organisationID = o.id
     JOIN Cities c ON o.cityID = c.id
+    JOIN Population p ON o.cityID = p.id AND p.year = @MostRecentYear
     JOIN EmissionStatusTypes est ON e.emissionStatusTypeID = est.id
     WHERE c.name = @CityName AND e.reportingYear = @MostRecentYear
-    GROUP BY e.id, est.type, e.description, e.comment;
 END;
 
-EXEC GetCityEmissionsWithStatus @CityName = 'Johannesburg';
+EXEC GetTotalEmissionByCity @CityName = 'Singapore';
 
 -----------------------------------------------------------
+IF OBJECT_ID('GetCityByStatus', 'P') IS NOT NULL
+BEGIN
+    DROP PROCEDURE GetCityByStatus;
+END
+GO
+
+CREATE PROCEDURE GetCityByStatus
+    @EmissionStatus VARCHAR(100)
+AS
+BEGIN
+    -- Determine the most recent reporting year across all emissions data
+    DECLARE @MostRecentYear INT;
+    SELECT @MostRecentYear = MAX(reportingYear) FROM GHG_Emissions;
+
+    -- Select cities and their emissions data for the most recent year matching the specified emission status
+    SELECT DISTINCT
+        c.id AS CityID,
+        c.name AS CityName,
+        e.reportingYear,
+        SUM(e.totalCityWideEmissionsCO2) AS TotalCityWideEmissionsCO2,
+        est.type AS EmissionStatus
+    FROM Cities c
+    JOIN Organisations o ON c.id = o.cityID
+    JOIN GHG_Emissions e ON o.id = e.organisationID
+    JOIN EmissionStatusTypes est ON e.emissionStatusTypeID = est.id
+    WHERE est.type = @EmissionStatus AND e.reportingYear = @MostRecentYear
+    GROUP BY c.id, c.name, e.reportingYear, est.type
+    ORDER BY c.name;
+END;
+
+
+EXEC GetCityByStatus @EmissionStatus = 'Increased';
